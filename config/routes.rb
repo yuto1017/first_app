0c299c56c7aad07c345b8f439cadaa908c770225
yuto1017 <yuto1017.oyama@gmail.com>
2024-02-24 18:19:36 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'posts', to: 'posts#index'
+  get 'posts/new', to: 'posts#new'
+  post 'posts', to: 'posts#create'
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
